4041c0f9f911964f4839eeca78e0654d4fc2c005
peifeng <1037212901@qq.com>
2022-09-23 21:54:22 +0800

    feat: v1.1.22

--- src/sns.js
+++ src/sns.js
@@ -276,9 +276,9 @@ export class SNS {
     const signer = await getSigner()
     const SNS = this.SNS.connect(signer)
     if(payWay == 1){
-      return await SNS.mintByMoreCoins([nameRemoveSuffix(name), payWay] ,{ value })
+      return await SNS.shortNameMint([nameRemoveSuffix(name), payWay] ,{ value })
     }else if(payWay == 2){
-      return await SNS.mintByMoreCoins([nameRemoveSuffix(name), payWay] ,{ value:0 })
+      return await SNS.shortNameMint([nameRemoveSuffix(name), payWay] ,{ value:0 })
     }else{
       return
     }
